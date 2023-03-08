550d990cb584fcf52d6fbfb5ac87d4e49f2f570c
Opilane TTHK <opilane@tthk.ee>
2023-03-08 14:16:07 +0200

    WindowsSize

--- SciCalculator/SciCalculator/App.xaml.cs
+++ SciCalculator/SciCalculator/App.xaml.cs
@@ -1,11 +1,33 @@
-﻿namespace SciCalculator;
+﻿#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+#endif
+
+namespace SciCalculator;
 
 public partial class App : Application
 {
-	public App()
-	{
-		InitializeComponent();
+    const int WindowWidth = 540;
+    const int WindowHeight = 1000;
 
-		MainPage = new AppShell();
-	}
+    public App()
+    {
+        InitializeComponent();
+
+#if WINDOWS
+		Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+		{
+			var mauiWindow = handler.VirtualView;
+			var nativeWindow = handler.PlatformView;
+			nativeWindow.Activate();
+			IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+			WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
+			AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+			appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
+		});
+#endif
+
+        MainPage = new AppShell();
+    }
 }
